728b4356cecfd2f6c1453733d2dc947d0fb7e8b3
UzairFarooqCG <115949991+UzairFarooq1@users.noreply.github.com>
2024-10-02 22:03:41 +0300

    feat: :rocket: Next-Auth & MongoDB setup

---
+++ app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,24 @@
+import client from "@/lib/db"
+import { MongoDBAdapter } from "@auth/mongodb-adapter"
+import NextAuth from "next-auth"
+import GoogleProvider from "next-auth/providers/google"
+
+
+
+
+export const authOptions = {
+  // Configure one or more authentication providers
+  providers: [
+    GoogleProvider({
+        clientId: process.env.GOOGLE_ID,
+        clientSecret: process.env.GOOGLE_SECRET,
+      }),
+    // ...add more providers here
+  ],
+  adapter: MongoDBAdapter(client),
+
+}
+
+const handler = NextAuth(authOptions)
+
+export { handler as GET, handler as POST}
--- app/page.js
+++ app/page.js
@@ -1,101 +1,44 @@
+'use client'
 import Image from "next/image";
+import { useSession, signIn, signOut } from "next-auth/react"
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const { data: session } = useSession()
+  let auth = {
+    isAuth: false,
+    name: null,
+    email: null,
+    image: null
+  }
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  if (session?.user) {
+    auth = {
+      isAuth: true,
+      name: session.user.name,
+      email: session.user.email,
+      image: session.user.image,
+    }
+  }
+
+  return (
+    auth.isAuth ?
+      <div className="flex items-center justify-center flex-col gap-4 bg-orange-500 h-screen">
+        <p>Logged in {auth.name}</p>
+        <button className="bg-white shadow-lg p-2 rounded-md" onClick={() => signOut('google')}>
+          Log Out with Google
+        </button>
+        {/* Display the image using the correct `auth.image` field */}
+        {auth.image && (
+          <Image src={auth.image} width="100" height="100" className=" rounded-full" alt={`${auth.name}'s profile picture`} />
+        )}
+      </div>
+      :
+      <div className="bg-blue-500 flex items-center justify-center h-screen">
+        <div className="bg-white px-4 p-2 rounded-lg shadow-md">
+          <button onClick={() => signIn('google')}>
+            Login with Google
+          </button>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+      </div>
+  )
 }
